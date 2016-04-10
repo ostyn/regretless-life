1098046179af8a9c453ee4716f0ddd9934f56e20
Jared Ostyn <ostyn@live.com>
2016-04-10 00:00:52 -0600

    Removing items from menu

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,8 +2,8 @@ export class App {
   configureRouter(config, router) {
     config.title = 'regretless.life';
     config.map([
-      { route: ['', 'blog'], moduleId: 'blog',nav: true, title: 'blog' },
-      { route: 'editor', moduleId: 'editor',nav: true, title: 'editor' },
+      { route: ['', 'blog'], moduleId: 'blog',nav: false, title: 'blog' },
+      { route: 'editor', moduleId: 'editor',nav: false, title: 'editor' },
       { route: 'flights', moduleId: 'flights', nav: true, title: 'flights' },
       { route: 'city/:name/explore', moduleId: 'city-page', nav: false, title: config.title },
       { route: 'about', moduleId: 'about', nav: true, title: 'about' }
