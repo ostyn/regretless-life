6bfd9b0f5282de4207ca972ef4eb13f327fdea7b
Jared Ostyn <ostyn@live.com>
2016-04-12 23:02:54 -0600

    Randomly found the correct aurelia-router feature for my search problem. Now should work for everything! Yay!

--- frontend/src/blog.js
+++ frontend/src/blog.js
@@ -1,7 +1,11 @@
 import {inject} from 'aurelia-framework';
 import {SkyScannerApi} from 'SkyScannerApi.js';
+import {activationStrategy} from 'aurelia-router';
 @inject(SkyScannerApi)
 export class Blog {
+    determineActivationStrategy(){
+        return activationStrategy.replace;
+    }
     activate(params) {
         if(params.query)
             return this.skyApi.findAllPosts(params.query);
