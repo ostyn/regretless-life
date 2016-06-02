c8d852b66b3cffa442fd1c7bccaccbadd95ff67c
Jared Ostyn <ostyn@live.com>
2016-06-01 23:45:36 -0600

    Visual tweaks and plural logic for comment(s) label

--- frontend/src/converters/trim-html-converter.js
+++ frontend/src/converters/trim-html-converter.js
@@ -1,6 +1,6 @@
 import trimHtml from 'trim-html';
 export class TrimHtmlValueConverter {
-  toView(value, length, suffix = " […]") {
+  toView(value, length, suffix = " <span class='grey small'>[…]</span>") {
     return trimHtml(value, { limit: length, suffix: suffix }).html;
   }
 }
