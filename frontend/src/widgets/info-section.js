b82fe5848ec14f24a3b2107ba35783d856c06473
Ostyn <ostyn@live.com>
2016-08-06 02:55:26 -0600

    Added new post button

--- frontend/src/widgets/info-section.js
+++ frontend/src/widgets/info-section.js
@@ -1,14 +1,23 @@
 import {inject} from 'aurelia-framework';
 import {AuthService} from 'aurelia-auth';
-@inject(AuthService)
+import {Router} from 'aurelia-router';
+@inject(AuthService, Router)
 export class InfoSection{
-    constructor(auth){
+    constructor(auth, router){
         this.auth = auth;
+        this.router = router;
     }
     logout(){
         this.auth.logout();
     }
     get isAuthenticated() {
         return this.auth.isAuthenticated();
+    }
+    me(){
+        if(this.isAuthenticated)
+            return this.auth.getMe();
+    }
+    newPost(){
+        this.router.navigateToRoute('editor');
     }
 }
