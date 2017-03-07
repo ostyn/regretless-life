b2a2deff84ee917c2970f88c9c3748c180f24f6c
Jared Ostyn <ostyn@live.com>
2017-03-07 15:53:26 +0700

    Improving our GA logging

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,7 +12,21 @@ export function configure(aurelia) {
           apiKey: 'AIzaSyDJUe-5GYpgt4u034NjKCa7qlWm4_TPsQ4',
           apiLibraries: 'drawing,geometry' //get optional libraries like drawing, geometry, ... - comma seperated list
       });
-    });
+    })
+    .plugin('aurelia-google-analytics', config => {
+      config.init('UA-82516304-1');
+      config.attach({
+        logging: {
+          enabled: false // Set to `true` to have some log messages appear in the browser console.
+        },
+        pageTracking: {
+          enabled: true // Set to `false` to disable in non-production environments.
+        },
+        clickTracking: {
+          enabled: false // Set to `false` to disable in non-production environments.
+        }
+      });
+    });;
 	
   aurelia.start().then(a => a.setRoot());
 }
