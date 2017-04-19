4874f71afd2e9abde8fa72d2f413f73a33907764
Jared Ostyn <ostyn@live.com>
2017-04-19 16:24:14 +0800

    Noticed still pointing at http. Switching to https

--- frontend/src/attributes/oneDriveProxyOnError.js
+++ frontend/src/attributes/oneDriveProxyOnError.js
@@ -2,11 +2,11 @@ import {inject} from 'aurelia-framework';
 
 @inject(Element)
 export class OneDriveProxyOnErrorCustomAttribute {
-  markdownString = 'onerror="this.onerror=null; this.src=\'http://regretless.life/data/oneDriveImageProxy?url=\' + encodeURIComponent(this.src);"';
+  markdownString = 'onerror="this.onerror=null; this.src=\'https://regretless.life/data/oneDriveImageProxy?url=\' + encodeURIComponent(this.src);"';
   constructor(element) {
     element.onerror=()=>{
         element.onerror=null;
-        element.src='http://regretless.life/data/oneDriveImageProxy?url=' + encodeURIComponent(element.src);
+        element.src='https://regretless.life/data/oneDriveImageProxy?url=' + encodeURIComponent(element.src);
     }
   }
 }
