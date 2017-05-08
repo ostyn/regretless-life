28832bcc4746de6b8e64a440bdad099a69ab8c3d
Jared Ostyn <ostyn@live.com>
2017-05-08 23:45:10 +0800

    Moving user routes to separate module

--- backend/backend.py
+++ backend/backend.py
@@ -2,17 +2,17 @@ import pymongo
 import blogPostModule
 import oneDriveModule
 import subscriptionModule
-from flask import Flask, request, jsonify
+import userModule
+
+from flask import Flask
 from flask_cors import CORS
-from flask_jwt import JWT, jwt_required, current_identity
+from flask_jwt import JWT
 from flask_mail import Mail
 from flask_compress import Compress
 
-from bson.objectid import ObjectId
 from authModule import AuthModule
 
 from configMaster import SMTP_USER, SMTP_PASSWORD, GOOGLE_MAPS_KEY
-from utilityClass import getDateInMilliseconds
 
 app = Flask(__name__)
 
@@ -36,22 +36,7 @@ compress = Compress(app)
 app.register_blueprint(blogPostModule.construct_blueprint(postsCollection, emailsCollection, mail))
 app.register_blueprint(oneDriveModule.construct_blueprint())
 app.register_blueprint(subscriptionModule.construct_blueprint(emailsCollection, mail))
-
-@app.route("/getAvailableUsers", methods=['GET'])
-@jwt_required()
-def getAvailableUsers():
-    users = list(usersCollection.find({},{'_id':1,'name':1}))
-    return jsonify({'resp':users})
-
-@app.route("/auth/me", methods=['GET'])
-@jwt_required()
-def getCurrentUser():
-    return jsonify({'resp':current_identity.name})
-
-@app.route("/register", methods=['POST', 'OPTION'])
-@jwt_required()
-def registerUser():
-    return authModule.registerUser(request)
+app.register_blueprint(userModule.construct_blueprint(usersCollection, authModule))
 
 if __name__ == "__main__":
     app.run(debug = True, port = 5000, host='0.0.0.0')
