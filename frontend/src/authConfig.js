0c524f46d3163fb8dd468751c99f40f11d2be31b
Jared Ostyn <ostyn@live.com>
2017-06-26 11:45:27 +0900

    Removed login/logout redirects

--- frontend/src/authConfig.js
+++ frontend/src/authConfig.js
@@ -4,6 +4,8 @@ var config = {
   // Our Node API is being served from localhost:3001
   baseUrl: 'https://regretless.life/data',
   // The API specifies that new users register at the POST /users enpoint.
+  loginRedirect: null,
+  logoutRedirect: null,
   signupUrl: 'register',
   signupRedirect: '#/login',
   // Logins happen at the POST /sessions/create endpoint.
