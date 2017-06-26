e9cdbf42db20d1784a6f94427aee22a5b947146f
Jared Ostyn <ostyn@live.com>
2017-06-26 11:46:31 +0900

    Removing comments from original config

--- frontend/src/authConfig.js
+++ frontend/src/authConfig.js
@@ -1,17 +1,12 @@
 var config = {
   authHeader: 'Authorization',
   authToken: 'JWT',
-  // Our Node API is being served from localhost:3001
   baseUrl: 'https://regretless.life/data',
-  // The API specifies that new users register at the POST /users enpoint.
   loginRedirect: null,
   logoutRedirect: null,
   signupUrl: 'register',
   signupRedirect: '#/login',
-  // Logins happen at the POST /sessions/create endpoint.
   loginUrl: 'auth',
-  // The API serves its tokens with a key of id_token which differs from
-  // aureliauth's standard.
   tokenName: 'access_token'
 }
 
