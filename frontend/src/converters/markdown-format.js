ed46c82b0ab16697ee8b8f6249c53d15779a1e9b
Jared Ostyn <ostyn@live.com>
2017-11-09 17:12:57 -0700

    Removing image proxy logic

--- frontend/src/converters/markdown-format.js
+++ frontend/src/converters/markdown-format.js
@@ -1,26 +1,11 @@
 import showdown from 'showdown';
 import youtube from 'showdown-youtube';
 import {inject} from 'aurelia-framework';
-import {OneDriveProxyOnErrorCustomAttribute} from 'attributes/oneDriveProxyOnError'
-@inject(OneDriveProxyOnErrorCustomAttribute)
 export class MarkdownFormatValueConverter {
-  constructor(oneDriveProxy) {
+  constructor() {
     showdown.setOption('tables', 'true');
     showdown.setOption('simplifiedAutoLink', 'true');
-    let imgRegex = /(?:<p>)?<img.*?src="(.+?)"(.*?)\/?>(?:<\/p>)?/gi;
-    showdown.extension('imageProxy', function () {
-      return [
-        {
-          type: 'output',
-          filter: function (text, converter, options) {
-            return text.replace(imgRegex, function (match, url, rest) {               
-                return match.replace("<img ", "<img " + oneDriveProxy.markdownString + " ");
-            });
-          }
-        }
-      ];
-    });
-    this.converter = new showdown.Converter({extensions: ['youtube', 'imageProxy']});
+    this.converter = new showdown.Converter({extensions: ['youtube']});
   }
   toView(value) {
     if(!value)
