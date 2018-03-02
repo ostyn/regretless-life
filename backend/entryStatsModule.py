69160f834060d7f94cf39126540f304f8072db2d
Jared Ostyn <ostyn@live.com>
2018-03-02 00:12:13 -0700

    Adding some stats endpoints

--- backend/entryStatsModule.py
+++ backend/entryStatsModule.py
@@ -5,6 +5,7 @@ def construct_blueprint(entriesCollection):
     entryStatsModule = Blueprint('entryStatsModule', __name__)
 
     @entryStatsModule.route("/getEntriesFromMonthAndYear", methods=['GET'])
+    @jwt_required()
     def getEntriesFromMonthAndYear():
         month = request.args.get('month')
         if (month):
@@ -16,6 +17,7 @@ def construct_blueprint(entriesCollection):
         return jsonify({'resp':entries})
 
     @entryStatsModule.route("/getActivityCount", methods=['GET'])
+    @jwt_required()
     def getActivityCount():
         month = request.args.get('month')
         if (month):
@@ -23,20 +25,71 @@ def construct_blueprint(entriesCollection):
         else:
             month = ""
         year = request.args.get('year',"")
-        mapc = Code("function () {"
-            "var keys = Object.keys(this.activities);"
-            "  keys.forEach(function(key) {"
-            "    emit(key, this.activities[key]);"
-            "  }.bind(this));"
-            "}")
-        reducec = Code("function (key, values) {"
-               "  var total = 0;"
-               "  for (var i = 0; i < values.length; i++) {"
-               "    total += values[i];"
-               "  }"
-               "  return total;"
-               "}")
-        entries = list(entriesCollection.map_reduce(mapc, reducec, "activity_counts", query={"date":{ "$regex" : "^" + year + "-" + month}}).find())
+        mapc = Code("""function () {
+            var keys = Object.keys(this.activities);
+              keys.forEach(function(key) {
+                emit(key, this.activities[key]);
+              }.bind(this));
+            }""")
+        reducec = Code("""function (key, values) {
+                 var total = 0;
+                 for (var i = 0; i < values.length; i++) {
+                   total += values[i];
+                 }
+                 return total;
+               }""")
+        entries = entriesCollection.map_reduce(mapc, reducec, "activity_counts", 
+            query={"date":{ "$regex" : "^" + year + "-" + month}}).find()
+        entries = sorted(entries, key=lambda d: d['value'], reverse=True)
+        return jsonify({'resp':entries})
+
+    @entryStatsModule.route("/getDaysForActivity", methods=['GET'])
+    @jwt_required()
+    def getDaysForActivity():
+        activityId = request.args.get('activityId')
+        month = request.args.get('month')
+        if (month):
+            month = month.zfill(2)
+        else:
+            month = ""
+        year = request.args.get('year',"")
+        mapc = Code("""function () {
+            var keys = Object.keys(this.activities);
+              keys.forEach(function(key) {
+               if(activityId === key)
+                emit(this.date, this.activities[key]);
+              }.bind(this));
+            }""")
+        reducec = Code("""function (key, values) {
+                 var total = 0;
+                 for (var i = 0; i < values.length; i++) {
+                   total += values[i];
+                 }
+                 return total;
+               }""")
+        entries = list(entriesCollection.map_reduce(mapc, reducec, "activity_counts", 
+            query={"date":{ "$regex" : "^" + year + "-" + month}},
+            scope={"activityId": activityId}).find())
+        return jsonify({'resp':entries})
+
+    @entryStatsModule.route("/getMoodEachDay", methods=['GET'])
+    @jwt_required()
+    def getMoodEachDay():
+        month = request.args.get('month')
+        if (month):
+            month = month.zfill(2)
+        else:
+            month = ""
+        year = request.args.get('year',"")
+        mapc = Code("""function () {
+                emit(this.date, this.mood);
+            }""")
+        reducec = Code("""function (key, values) {
+                 return values[0];//This doesn't really average moods
+               }""")
+        entries = entriesCollection.map_reduce(mapc, reducec, "activity_counts", 
+            query={"date":{ "$regex" : "^" + year + "-" + month}}).find()
+        entries = sorted(entries, key=lambda d: d['_id'])
         return jsonify({'resp':entries})
 
     return entryStatsModule
