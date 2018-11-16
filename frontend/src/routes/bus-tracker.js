5aa0de0db587f191eabff4e8c3a103abd93728af
Jared Ostyn <ostyn@live.com>
2018-11-16 00:05:54 -0700

    Reorganizing UI and adding routeparams for route and stop

--- frontend/src/routes/bus-tracker.js
+++ frontend/src/routes/bus-tracker.js
@@ -1,25 +1,59 @@
-import { inject } from 'aurelia-framework';
+import { inject, observable } from 'aurelia-framework';
 import { VrtDao } from "dao/VrtDao";
 @inject(VrtDao)
 export class BusTracker {
-    constructor(vrtDao) {
-        this.vrtDao = vrtDao;
-        this.vrtDao.getRoutes()
-            .then((routes) => {
-                this.routes = routes;
-            });
-
-    }
-    setRoute(route){
-        this.vrtDao.getStopsOnRoute(route)
+    @observable selectedRoute = undefined;
+    @observable selectedStop = undefined;
+    notCurrentlyRequesting = true;
+    selectedRouteChanged(newVal, oldVal) {
+        this.stops = undefined;
+        this.vrtDao.getStopsOnRoute(this.selectedRoute.routeId)
             .then((stops) => {
                 this.stops = stops;
             });
     }
-    setStop(stop){
-        this.vrtDao.getStatusForStop(stop)
+    selectedStopChanged(newVal, oldVal) {
+        this.currentStopInfo = undefined;
+        this.vrtDao.getStatusForStop(this.selectedStop.stopId)
             .then((info) => {
                 this.currentStopInfo = info;
             });
+    }
+
+    activate(params, routeConfig, navigationInstruction) {
+        this.vrtDao.getRoutes()
+            .then((routes) => {
+                routes.data.forEach(route => {
+                    if (route.routeId == params.routeId)
+                        this.selectedRoute = route;
+                });
+                this.routes = routes;
+            });
+        this.vrtDao.getStopsOnRoute(params.routeId)
+            .then((stops) => {
+                stops.data.forEach(stop => {
+                    if (stop.stopId == params.stopId)
+                        this.selectedStop = stop;
+                });
+                this.stops = stops;
+            });
+            setInterval(this.getStatusForCurrentStop, 10000);
+        ;
+    }
+
+    getStatusForCurrentStop = () => {
+        if (this.notCurrentlyRequesting) {
+            this.notCurrentlyRequesting = false;
+            this.vrtDao.getStatusForStop(this.selectedStop.stopId)
+                .then((info) => {
+                    this.notCurrentlyRequesting = true;
+                    this.currentStopInfo = info;
+                });
+        }
+    }
+
+
+    constructor(vrtDao) {
+        this.vrtDao = vrtDao;
     }
 }
