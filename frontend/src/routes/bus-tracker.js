26d57bcd4f25e94d97e75281bad657b08039df3b
Jared Ostyn <ostyn@live.com>
2019-01-25 06:44:22 -0700

    Messing with the request ordering and making query params always sync

--- frontend/src/routes/bus-tracker.js
+++ frontend/src/routes/bus-tracker.js
@@ -1,7 +1,10 @@
 import { inject, observable } from 'aurelia-framework';
 import { VrtDao } from "dao/VrtDao";
-@inject(VrtDao)
+import {Router, activationStrategy} from 'aurelia-router';
+@inject(VrtDao, Router)
 export class BusTracker {
+    currentStop = undefined;
+    currentRoute = undefined;
     @observable selectedRoute = undefined;
     @observable selectedStop = undefined;
     notCurrentlyRequesting = true;
@@ -9,46 +12,51 @@ export class BusTracker {
     selectedRouteChanged(newVal, oldVal) {
         this.stops = undefined;
         if (this.selectedRoute) {
-            this.vrtDao.getStopsOnRoute(this.selectedRoute.routeId)
+            this.currentRoute = this.selectedRoute.routeId;
+            this.vrtDao.getStopsOnRoute(this.currentRoute)
                 .then((stops) => {
+                    stops.data.forEach(stop => {
+                        if (stop.stopId == this.currentStop)
+                            this.selectedStop = stop;
+                    });
                     this.stops = stops;
                 });
         }
     }
     selectedStopChanged(newVal, oldVal) {
-        this.currentStopInfo = undefined;
         if (this.selectedStop) {
+            this.currentStopInfo = undefined;
+            this.currentStop = this.selectedStop.stopId;
             if (this.interval)
                 clearInterval(this.interval);
             this.getStatusForCurrentStop();
             this.interval = setInterval(this.getStatusForCurrentStop, 10000);
+            this.router.navigate(this.router.generate('bus', {
+                stopId : this.currentStop,
+                routeId : this.currentRoute
+            }));
         }
     }
-
+    determineActivationStrategy(){
+        return activationStrategy.noChange;
+    }
     activate(params, routeConfig, navigationInstruction) {
+        this.currentRoute = params.routeId;
+        this.currentStop = params.stopId;
         this.vrtDao.getRoutes()
             .then((routes) => {
                 routes.data.forEach(route => {
-                    if (route.routeId == params.routeId)
+                    if (route.routeId == this.currentRoute)
                         this.selectedRoute = route;
                 });
                 this.routes = routes;
             });
-        if (params.routeId)
-            this.vrtDao.getStopsOnRoute(params.routeId)
-                .then((stops) => {
-                    stops.data.forEach(stop => {
-                        if (stop.stopId == params.stopId)
-                            this.selectedStop = stop;
-                    });
-                    this.stops = stops;
-                });
     }
 
     getStatusForCurrentStop = () => {
-        if (this.notCurrentlyRequesting) {
+        if (this.currentStop && this.notCurrentlyRequesting) {
             this.notCurrentlyRequesting = false;
-            this.vrtDao.getStatusForStop(this.selectedStop.stopId)
+            this.vrtDao.getStatusForStop(this.currentStop)
                 .then((info) => {
                     this.notCurrentlyRequesting = true;
                     this.currentStopInfo = info;
@@ -60,8 +68,9 @@ export class BusTracker {
             this.timeSinceLastDataTimestamp = Math.round((new Date().getTime() - new Date(this.currentStopInfo.time).getTime())/1000);
     }
 
-    constructor(vrtDao) {
+    constructor(vrtDao, router) {
         this.vrtDao = vrtDao;
+        this.router = router;
         setInterval(() => this.updateTimeSinceLastDataTimestamp(), 1000);
     }
 }
