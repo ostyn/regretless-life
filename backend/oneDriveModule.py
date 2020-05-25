924e217293099661a7db5fc38d1b49506c5bfbc4
Jared Ostyn <ostyn@live.com>
2020-05-24 23:09:58 -0600

    Removing unused endpoint

--- backend/oneDriveModule.py
+++ backend/oneDriveModule.py
@@ -1,8 +1,6 @@
-import requests
 from furl import furl
-from urllib.request import urlopen, unquote
-from urllib.parse import urlparse
-from flask import Blueprint, request, jsonify, Response, stream_with_context
+from urllib.request import urlopen
+from flask import Blueprint, request, jsonify
 
 def construct_blueprint():
     oneDriveModule = Blueprint('oneDriveModule', __name__)
@@ -13,16 +11,4 @@ def construct_blueprint():
         finalurl = res.geturl()
         furled = furl(finalurl)
         return jsonify({'authkey':furled.args['authkey']})
-
-    @oneDriveModule.route('/oneDriveImageProxy', methods=['GET'])
-    def oneDriveImageProxy():
-        url = unquote(request.args.get('url'))
-        parsedUrl = urlparse(url)
-        if not (parsedUrl.netloc.endswith('livefilestore.com')):
-            return jsonify({'error':"Image source not supported."})
-        req = requests.get(url, stream = True)
-        resp = Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
-        resp.headers['Expires'] = req.headers['Expires']
-        resp.headers['Cache-Control'] = req.headers['Cache-Control']
-        return resp
     return oneDriveModule
