2ca414bd964c0d94757b5c2f9d8c1dc9fbcfa512
Jared Ostyn <ostyn@live.com>
2020-05-26 16:33:30 -0600

    First stab at function for vector map data

--- frontend/functions/index.js
+++ frontend/functions/index.js
@@ -1,8 +1,39 @@
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const Firestore = require('@google-cloud/firestore');
+const PROJECTID = 'regretless-life-test';
+const COLLECTION_NAME = 'posts';
+const firestore = new Firestore({
+  projectId: PROJECTID,
+  timestampsInSnapshots: true,
+});
+exports.getPostsByYearAndLocation = functions.https.onRequest((req, res) => {
+    return firestore.collection(COLLECTION_NAME)
+    .get()
+    .then(snapshot => {
+        let years = {};
+        snapshot.forEach(doc => {
+            const post = {
+                ...doc.data(),
+                _id: doc.id
+            };
+            if(post.locationInfo === undefined)
+                return;
+            post["year"] = new Date(post.date).getFullYear();
+            if(years[post.year] === undefined)
+                years[post.year] = {};
+            if(years[post.year][post.locationInfo.countryCode] === undefined)
+            {
+                years[post.year][post.locationInfo.countryCode] = {};
+                years[post.year][post.locationInfo.countryCode].countryCode = post.locationInfo.countryCode;
+                years[post.year][post.locationInfo.countryCode].country = post.locationInfo.country;
+            }
+            if(years[post.year][post.locationInfo.countryCode].posts === undefined)
+                years[post.year][post.locationInfo.countryCode].posts = [];
+            years[post.year][post.locationInfo.countryCode].posts.push(post);
+        });
+      return res.status(200).send(years);
+    }).catch(err => {
+      console.error(err);
+      return res.status(404).send({ error: 'Unable to retrieve the document' });
+    });
+});
