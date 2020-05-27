5efefdc2162874f3ed0ad0e3d93731f85f33686a
Jared Ostyn <ostyn@live.com>
2020-05-26 21:37:46 -0600

    Fixing CORS issues on function

--- frontend/functions/index.js
+++ frontend/functions/index.js
@@ -1,42 +1,44 @@
 const functions = require('firebase-functions');
 const Firestore = require('@google-cloud/firestore');
+const cors = require('cors')({ origin: true });
 const PROJECTID = 'regretless-life-test';
 const COLLECTION_NAME = 'posts';
 const firestore = new Firestore({
-  projectId: PROJECTID,
-  timestampsInSnapshots: true,
+    projectId: PROJECTID,
+    timestampsInSnapshots: true,
 });
 exports.getPostsByYearAndLocation = functions.https.onRequest((req, res) => {
-    return firestore.collection(COLLECTION_NAME)
-    .get()
-    .then(snapshot => {
-        let years = {};
-        snapshot.forEach(doc => {
-            const fullPost = doc.data();
-            if(fullPost.locationInfo === undefined)
-                return;
-            const post = {
-                year: new Date(fullPost.date).getFullYear(),
-                date: fullPost.date,
-                title: fullPost.title,
-                location: fullPost.locationInfo.name,
-                id: doc.id
-            };
-            if(years[post.year] === undefined)
-                years[post.year] = {};
-            if(years[post.year][fullPost.locationInfo.countryCode] === undefined)
-            {
-                years[post.year][fullPost.locationInfo.countryCode] = {};
-                years[post.year][fullPost.locationInfo.countryCode].countryCode = fullPost.locationInfo.countryCode;
-                years[post.year][fullPost.locationInfo.countryCode].country = fullPost.locationInfo.country;
-            }
-            if(years[post.year][fullPost.locationInfo.countryCode].posts === undefined)
-                years[post.year][fullPost.locationInfo.countryCode].posts = [];
-            years[post.year][fullPost.locationInfo.countryCode].posts.push(post);
-        });
-      return res.status(200).send(years);
-    }).catch(err => {
-      console.error(err);
-      return res.status(404).send({ error: 'Unable to retrieve the document' });
-    });
+    cors(req, res, () => {
+        return firestore.collection(COLLECTION_NAME)
+            .get()
+            .then(snapshot => {
+                let years = {};
+                snapshot.forEach(doc => {
+                    const fullPost = doc.data();
+                    if (fullPost.locationInfo === undefined)
+                        return;
+                    const post = {
+                        year: new Date(fullPost.date).getFullYear(),
+                        date: fullPost.date,
+                        title: fullPost.title,
+                        location: fullPost.locationInfo.name,
+                        id: doc.id
+                    };
+                    if (years[post.year] === undefined)
+                        years[post.year] = {};
+                    if (years[post.year][fullPost.locationInfo.countryCode] === undefined) {
+                        years[post.year][fullPost.locationInfo.countryCode] = {};
+                        years[post.year][fullPost.locationInfo.countryCode].countryCode = fullPost.locationInfo.countryCode;
+                        years[post.year][fullPost.locationInfo.countryCode].country = fullPost.locationInfo.country;
+                    }
+                    if (years[post.year][fullPost.locationInfo.countryCode].posts === undefined)
+                        years[post.year][fullPost.locationInfo.countryCode].posts = [];
+                    years[post.year][fullPost.locationInfo.countryCode].posts.push(post);
+                });
+                return res.status(200).send({data:years});
+            }).catch(err => {
+                console.error(err);
+                return res.status(404).send({ error: 'Unable to retrieve the document' });
+            });
+    })
 });
